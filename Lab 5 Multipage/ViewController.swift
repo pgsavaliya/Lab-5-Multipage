0e9c322e99bbd1a6c969766e722791576fd8c537
Pgsavaliya <pgsavaliya01@gmail.com>
2024-02-11 20:47:06 -0500

    Initial Commit

---
+++ Lab 5 Multipage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab 5 Multipage
+//
+//  Created by Pavan savaliya on 2024-02-11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
